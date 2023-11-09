f339c81277baf159cc00e61810bc2b2fd1e28745
Yaroslav <yaroslav.roro@gmail.com>
2023-11-09 20:53:29 +0200

    fist js

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+alert('hello world')
+
+let goodDayNumber = 2;
+console.log('Your good day number is ' + goodDayNumber);
+goodDayNumber =13;
+console.log('Your good day number is ' + goodDayNumber);
+
+const badDayNumber = null;
+console.log('Your bad day number is ' + badDayNumber);
+badDayNumber = 32;
+console.log('Your bad day number is ' + badDayNumber);
