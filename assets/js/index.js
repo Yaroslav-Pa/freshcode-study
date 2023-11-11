4e469911392a667940546cb3bc30d1d08ee8ec41
Yaroslav <yaroslav.roro@gmail.com>
2023-11-11 12:04:15 +0200

    HW-done

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,34 @@
-alert('hello world')
+const num1 = 10;
+const num2 = 50;
 
-let goodDayNumber = 2;
-console.log('Your good day number is ' + goodDayNumber);
-goodDayNumber =13;
-console.log('Your good day number is ' + goodDayNumber);
+const task1 = num1+num2;
+console.log('num1+num2 = ' + task1);
 
-const badDayNumber = null;
-console.log('Your bad day number is ' + badDayNumber);
-badDayNumber = 32;
-console.log('Your bad day number is ' + badDayNumber);
+const task2 = task1-50;
+console.log('task1-50 = ' + task2);
+
+const task3 = task2*task1;
+console.log('task2*task1 = ' + task3);
+
+const task4_1 = task3/2.687;
+console.log('task3/2.687 = ' + task4_1);
+const task4_2 = task3%2.687;
+console.log('task3%2.687 = ' + task4_2);
+
+const task5 = !!task1;
+console.log('!!task1 = ' + task5);
+
+
+let num = 5;
+
+num +=10;
+console.log('\n+10= '+num);
+
+num *= 2;
+console.log('*2= '+num);
+
+num -= 4;
+console.log('-4= '+num);
+
+num /= 6;
+console.log('/6= '+num);
