ce7654de1d186f590fb71fd7478c15da2eb871ef
Yaroslav <yaroslav.roro@gmail.com>
2023-12-22 20:58:11 +0200

    CW+HW 22.12

--- assets/js/index.js
+++ assets/js/index.js
@@ -22,4 +22,47 @@ const clicking = () => {
   return clickFun;
 };
 
-document.getElementById('clicableButton').addEventListener('click', clicking());
+// const button1 = document.getElementById('clicableButton');
+// button1.addEventListener('click', clicking());
+
+
+
+
+
+//22.12.23
+
+// пошук елемента у ДОМ - дереві
+/*
+document.getElementsByTagName('div') - шукає усі елементи з указаним тегом і кладе іх у псевдомасив (є ітератор)
+* document.getElementById('someId') - повертає перший елемент з вказаним айді
+document.getElementsByClassName('className') - шукає усі елементи з указаним класом
+document.getElementsByName('name') - шукає усі елементи з указаним атрибутом name
+
+* document.querySelector('#div.btn > span') - повертає перший елемент який задовільняє вказаному css селектору
+document.querySelectorAll('#div.btn > span') - повертає всі елементи які задовільняють вказаному css селектору
+*/
+
+const button1 = document.getElementById('clicableButton');
+
+
+const finded = document.querySelectorAll(['div>section>h1', 'div>img', 'body>section>p']);
+const findedImg = document.querySelector('div>img');
+const findedP = document.querySelector('body>section>p');
+
+// const onceClick = () => {
+//   alert('v2 once click')
+//   button1.removeEventListener('click', onceClick)
+// }
+// // button1.addEventListener('click', ()=>alert('v1 once click'), {once:true});
+// button1.addEventListener('click', onceClick);
+
+
+const getText = (event) => {
+  console.log(event.target.textContent);
+  console.log(event.target.innerText);
+}
+
+const setAskedNumberDouble = (event) => 
+  event.target.nextElementSibling.textContent += prompt('enter your number')**2
+
+button1.addEventListener('click', setAskedNumberDouble);
