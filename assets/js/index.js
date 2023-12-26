68ffa7fd656abf2d8b2051fa4c8c4efcc29f7b01
Yaroslav <yaroslav.roro@gmail.com>
2023-12-26 21:15:27 +0200

    CW HW done

--- assets/js/index.js
+++ assets/js/index.js
@@ -65,7 +65,7 @@ const getText = (event) => {
 const setAskedNumberDouble = (event) => 
   event.target.nextElementSibling.textContent += prompt('enter your number')**2
 
-button1.addEventListener('click', setAskedNumberDouble);
+// button1.addEventListener('click', setAskedNumberDouble);
 
 
 
@@ -77,7 +77,7 @@ button1.addEventListener('click', setAskedNumberDouble);
 
 
 
-// 25.11.23
+// 25.12.23
 
 
 const clickHandler = (event) => {
@@ -118,11 +118,11 @@ const clickHandler = (event) => {
 // -Делегування подій
 
 const divContainer = document.getElementById('container-buttons');
-divContainer.addEventListener('click',(e)=>{
-  if (e.target.tagName === 'BUTTON'){
-    divContainer.style.backgroundColor = e.target.textContent;
-  }
-})
+// divContainer.addEventListener('click',(e)=>{
+//   if (e.target.tagName === 'BUTTON'){
+//     divContainer.style.backgroundColor = e.target.textContent;
+//   }
+// })
 
 // form
 
@@ -146,22 +146,134 @@ divContainer.addEventListener('click',(e)=>{
 const form2 = document.getElementById('form');
 const paragraf = document.querySelector('form > p');
 
-form2.addEventListener('submit', (e) =>{
-  e.preventDefault();
+// form2.addEventListener('submit', (e) =>{
+//   e.preventDefault();
 
-  const inputVal = e.target.elements[0].value;
+//   const inputVal = e.target.elements[0].value;
 
-  // if (+inputVal === +inputVal & inputVal.trim() !== ''){
-  if (!isNaN(inputVal) & inputVal.trim() !== ''){ // isNaN неявно переводить типи у намбер
-    paragraf.style.color = 'black';
-    paragraf.textContent = `Doble of your input is ${inputVal**2}`;
-  }else{
-    paragraf.style.color = 'red';
-    paragraf.textContent = `Error input correct type (number)`;
-  } 
+//   // if (+inputVal === +inputVal & inputVal.trim() !== ''){
+//   if (!isNaN(inputVal) & inputVal.trim() !== ''){ // isNaN неявно переводить типи у намбер
+//     paragraf.style.color = 'black';
+//     paragraf.textContent = `Doble of your input is ${inputVal**2}`;
+//   }else{
+//     paragraf.style.color = 'red';
+//     paragraf.textContent = `Error input correct type (number)`;
+//   } 
   
-  // e.target.elements[0].value = '';
-  e.target.reset();
+//   // e.target.elements[0].value = '';
+//   e.target.reset();
+// })
+
+
+
+
+
+
+
+
+//// 26.12.23
+//// атрибути 
+
+
+// const img1 = document.getElementById('img');
+
+// img.alt = 'some alt';
+// img.src = 'https://imageio.forbes.com/specials-images/imageserve/6064b148afc9b47d022718d1/Hennessey-Venom-F5/960x0.jpg?height=473&width=711&fit=bounds';
+
+// встановлення атрибутів
+// paragraf.setAttribute('title', 'Title from DOM');
+
+// теж але через вузол
+// const parTitleNode = document.createAttribute('title');
+// parTitleNode.value = 'this will be title';
+// img1.setAttributeNode(parTitleNode);
+
+// отримати значення атрибута
+// const paragrafTitle = paragraf.getAttribute('title');
+
+// через вузол
+// const imgTitleNode = img1.getAttributeNode('title');
+
+
+
+
+///// test
+
+// const img = document.getElementById('testAttr');
+// const buttonToChange = document.getElementById('changeImg');
+
+// const srcArray = [
+//   {src : 'https://www.timeoutdubai.com/cloud/timeoutdubai/2021/09/11/hfpqyV7B-IMG-Dubai-UAE.jpg',
+//   alt :  'alt for img1'},
+//   {src : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmOaVtszzMbkEii_NNVzQ4o2Ue_ThhRxhzzw&usqp=CAU',
+//   alt :  'alt for img2'},
+// ]
+// let srcNode = document.createAttribute('src');
+// srcNode.value = srcArray[0].src;
+// let altNode = document.createAttribute('alt');
+// altNode.value = srcArray[0].alt;
+
+// img.setAttributeNode(srcNode); /// !!! була проблема задаванням значення потрібно саме використовувати .setAttributeNode()
+// img.setAttributeNode(altNode); /// !!!
+
+// buttonToChange.addEventListener('click', ()=>{
+//   if(img.src === srcArray[0].src) { /// !!!
+//     srcNode.value = srcArray[1].src; /// !!!
+//     altNode.value = srcArray[1].alt; /// !!!
+//   }else {
+//     srcNode.value = srcArray[0].src;
+//     altNode.value = srcArray[0].alt;
+//   }
+// })
+
+/*
+const dataBtn = document.getElementById('dataBtn');
+
+dataBtn.addEventListener('click', (e) => {
+  // const dataKeyText = dataBtn.getAttribute('data-key');
+  
+  // dataset - об'єкт у який складають усі атрибутиякі починаться з data-
+  console.log(dataBtn.dataset);
+  console.log(dataBtn.dataset.key);
+  console.log(dataBtn.dataset.someLongName);
+});
+
+
+const colorSection = document.getElementById('colorButtonsContainer');
+
+colorSection.addEventListener('click', (e)=>{
+  if (e.target.tagName === 'BUTTON'){
+    colorSection.style.backgroundColor = e.target.dataset.color;
+  }
+})*/
+
+
+//// додавання елементу на сторінку (Н. для v-for)
+
+// створення елементів через DOM
+const elem = document.createElement('div');
+elem.textContent = 'Im from DOM';
+elem.setAttribute('title', 'sdffds');
+// document.querySelector('section').appendChild(elem);
+// document.querySelector('section').append(elem, 'append'); //універнісальніше бо можна закидувати одразу декілька елементів
+// елемент "elem" може 
+
+///// ДЗ
+
+const doToForm = document.getElementById('toDoForm');
+const inputToDo = doToForm.elements[0];
+
+doToForm.addEventListener('submit', (e)=>{
+  e.preventDefault();
+  
+  if (!(inputToDo.value.trim() !== '')){
+    inputToDo.style.border = '2px solid red';
+    throw new Error('Task need to be not empty')
+  }
+
+  let paragraf = document.createElement('p');
+  paragraf.textContent = inputToDo.value;
+  inputToDo.style.border = '';
+  inputToDo.value = '';
+  document.body.append(paragraf);
 })
-
-
