babd1eebae7f5d4eb47ede3604749bfa394bdec0
Yaroslav <yaroslav.roro@gmail.com>
2024-01-08 22:58:25 +0200

    HW for 6.01

--- assets/js/HW/async.js
+++ assets/js/HW/async.js
@@ -1,14 +1,44 @@
-let users = {};
 const url = 'https://jsonplaceholder.typicode.com/users';
+const placeToSpawn = document.getElementById('grid');
 
-async function getData() {
-  try{
-    users =(await fetch(url)).json();
-  }catch(err){
-    throw new Error (err);
+async function createCardsAsyncAwait() {
+  try {
+    let userArr = await (await fetch(url)).json();
+    userArr.map((user) => createUserCard(user));
+  } catch (err) {
+    throw new Error(err);
   }
 }
-getData();
 
+function createCardsPromise() {
+  fetch(url)
+    .then((dataInJson) => dataInJson.json())
+    .then((data) =>
+      data.map((user) => {
+        createUserCard(user);
+      })
+    )
+    .catch((err) => new Error(err));
+}
 
-new Promise(fetch(url)).then((json)=>json.json()).then()
+const createUserCard = ({ name, email, website }) => {
+  const createdDiv = createElementWithText('', 'div', ['card']);
+  createdDiv.append(
+    createElementWithText(name),
+    createElementWithText(email),
+    createElementWithText(website)
+  );
+  createdDiv.addEventListener('click', (event)=>{
+    event.currentTarget.remove();
+  })
+  placeToSpawn.append(createdDiv);
+};
+
+const createElementWithText = (text, elemType = 'p', styleClasses = []) => {
+  const textElem = document.createElement(elemType);
+  textElem.textContent = text;
+  for (let style of styleClasses){
+    textElem.classList.add(style);
+  }
+  return textElem;
+};
