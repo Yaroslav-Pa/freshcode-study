cef9a01e7745930803cda44e6c655b4afcfd336c
Yaroslav <yaroslav.roro@gmail.com>
2024-01-16 20:06:07 +0200

    life cycle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import PhoneDashboard from './components/PhoneDashboard';
+import Clicker from './components/Clicker';
 
 function App(props) {
+  const [isClickerShown, setIsClickerShown] = useState(true)
   return (
     <>
+      <button onClick={()=>{setIsClickerShown(!isClickerShown)}}>{isClickerShown? 'Close' : 'Shown'} Clicker</button>
+      {isClickerShown && <Clicker/>}    
       <PhoneDashboard />
     </>
   );
