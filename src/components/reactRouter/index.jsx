6154c0d0bfc067b556a375e9a516a66be43dfad1
Yaroslav-Pa <yaroslav.roro@gmail.com>
2024-01-30 19:59:24 +0200

    done with react-router-dom

--- src/components/reactRouter/index.jsx
+++ src/components/reactRouter/index.jsx
@@ -7,15 +7,18 @@ function NewrRouter() {
       <BrowserRouter>
         {/*Switch перевіряє посимвольно тож потрібно home або у кінець або писати exact*/}
         <Switch>
+          {/* Всього 3 варіанти відображення: */}
+          {/* children, render, component */}
           <Route exact path="/">
             <Home />
           </Route>
-          <Route path="/Contacts">
-            <Contacts />
-          </Route>
-          <Route path="/Something">
-            <Something />
-          </Route>
+          <Route exact path="/Profile/:id" component={Profile}/>
+          <Route
+            path="/Contacts"
+            render={(routeProps) => <Contacts {...routeProps} />}
+          />
+          <Route path="/Something" component={Something} />
+          {/* routeProps це три об'єкта *доки*: history(перемішення туди сюди ), location(уся інфа по URL), match(інфо об'єкт: isExact (12строка), поточний юрл та пасс)*/}
         </Switch>
       </BrowserRouter>
     </>
@@ -48,13 +51,22 @@ function Something() {
     </>
   );
 }
+function Profile(props) {
+  return (
+    <>
+      <Navigation />
+      <h2>Profile {props.match.params.id}</h2>
+    </>
+  );
+}
 function Navigation() {
   return (
     <>
-    {/* NavLink знає на якої сторінці ти сидиш, потрібно наприклад для змінення стилів якщо ти на цій сторінці */}
-      <NavLink to="/">Home</NavLink> 
+      {/* NavLink знає на якої сторінці ти сидиш, потрібно наприклад для змінення стилів якщо ти на цій сторінці */}
+      <NavLink to="/">Home</NavLink>
       <Link to="/Contacts">Contacts</Link>
       <Link to="/Something">Something</Link>
+      <Link to="/Profile/12312">Profile</Link>
     </>
   );
 }
