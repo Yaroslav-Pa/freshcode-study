b539ebb9b7b886422c16e6014ce28714359918e0
Yaroslav-Pa <yaroslav.roro@gmail.com>
2024-02-20 21:06:46 +0200

    ddl dml

---
+++ ddl/create_table.sql
@@ -0,0 +1,32 @@
+/*
+  текстові типи даних:
+  TEXT = string у JS (необмежено про розмірам)
+  BPCHAR -> необмежено про розмірам але видаляються пробіли
+  CHAR (n) -> рядок фіксованої довжини до n символів
+    CHAR (6)
+      'test12' -> 'test12'
+      'test1234' -> 'test12'
+      'te' -> 'te    '
+  VARCHAR (n) -> рядок фіксованої змінної довжини до n символів
+    VARCHAR (6)
+      'test12' -> 'test12'
+      'test1234' -> 'test12'
+      'te' -> 'te'
+  INT === INTEGER
+  точність - загальна кількість цифр у числі
+  масштаб - кількість цифр після крапки в числі
+  NUMERIC (точність, масштаб)
+  NUMERIC === DECIMAL
+*/
+
+CREATE TABLE users(
+  -- опис структури таблиці
+  --name DOMAIN
+  id SERIAL,
+  first_name VARCHAR(80),
+  last_name VARCHAR(80),
+  balance NUMERIC(13,2),
+  is_male BOOLEAN,
+  birthday DATE
+
+)
---
+++ dml/insert.sql
@@ -0,0 +1,15 @@
+-- додвання даних у рядки
+INSERT INTO users
+VALUES(1, 'First', 'User', 5000.01, false, '2007-07-07');
+
+-- декілька користувачів
+INSERT INTO users
+VALUES
+(2, 'Test', '2', 65465.12, false, '1996-4-4'),
+(3,'Other','User', 0, true, '1980-6-7');
+
+-- вказання порядку вставки даних
+INSERT INTO users 
+(balance, first_name, last_name, is_male)
+VALUES
+(1500, 'first', 'last', false);
