1f2a00d4897b79bd46cec0023cdff29c6940cfe0
Yaroslav-Pa <yaroslav.roro@gmail.com>
2024-02-20 13:22:38 +0200

    clear start

--- validation/userSchemas.js
+++
@@ -1,7 +0,0 @@
-const yup = require('yup');
-
-module.exports.REGISTRATION_SCHEMA = yup.object({
-  email: yup.string().email().required(),
-  password: yup.string().min(8).max(16),
-  gender: yup.string()
-});
