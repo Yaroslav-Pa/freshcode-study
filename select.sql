2aecddaa66f4412aa1926b9afa2c543dbebff24e
Yaroslav-Pa <yaroslav.roro@gmail.com>
2024-02-23 23:29:32 +0200

    some improvements

--- select.sql
+++ select.sql
@@ -6,5 +6,5 @@ SELECT * FROM employees WHERE full_name = 'Петро';
 SELECT * FROM employees WHERE full_name != 'Петро';
 --
 SELECT * FROM employees WHERE (EXTRACT(YEAR from current_timestamp)-EXTRACT(YEAR from birthday)=27 OR salary = 1000);
-SELECT * FROM employees WHERE (EXTRACT(YEAR from current_timestamp)-EXTRACT(YEAR from birthday) IN (26, 27, 28));
-SELECT * FROM employees WHERE (EXTRACT(YEAR from current_timestamp)-EXTRACT(YEAR from birthday)>23 AND EXTRACT(YEAR from current_timestamp)-EXTRACT(YEAR from birthday)<27) OR (salary>400 AND salary<1000);
+SELECT * FROM employees WHERE EXTRACT(year from age(birthday)) IN (26, 27, 28);
+SELECT * FROM employees WHERE (EXTRACT(year from age(birthday)) > 23 AND EXTRACT(year from age(birthday)) < 27) OR (salary > 400 AND salary < 1000);
