0577d64e7248cdb92d7ac4c784405329e79e2732
Yaroslav-Pa <yaroslav.roro@gmail.com>
2024-02-27 18:00:28 +0200

    update rep from Kirilo

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,46 @@
 const { Client } = require('pg');
+const fs = require('fs/promises');
+const { getUsers } = require('../api');
+const { mapUsers } = require('../utils/userUtils');
+const { user, password, host, port, database } = require('../config/db.json');
+
 const config = {
-  user: 'postgres',
-  password: '1',
-  host: 'localhost',
-  database: 'mylessons',
-  port: 5432,
+  user,
+  password,
+  host,
+  port,
+  database,
 };
+
 const client = new Client(config);
-const start = async () => {
+
+async function start() {
+  const users = await getUsers();
+
   await client.connect();
 
-  const result = await client.query('SELECT * FROM users;');
-  console.log(result);
+  const resetQuery = await fs.readFile(`${__dirname}/sql/reset.sql`, 'utf-8');
+
+  await client.query(resetQuery);
+
+  const result = await client.query(`INSERT INTO users (
+    "first_name",
+    "last_name",
+    "email",
+    "is_male",
+    "balance",
+    "height",
+    "weight",
+    "comments",
+    "birthday"
+  )
+  VALUES
+  ${mapUsers(users)}
+  RETURNING *;`);
+  // користувачі
+  console.log(result.rows);
 
   await client.end();
-};
+}
+
 start();
