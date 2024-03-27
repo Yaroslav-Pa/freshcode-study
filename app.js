c1f5f6a0cae2e5c6bda613ca3750dcaa33460d56
Yaroslav-Pa <yaroslav.roro@gmail.com>
2024-03-27 17:32:13 +0200

    single source path for public

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = require('./routers');
+const CONSTANTS = require('./constants');
 const app = express();
 
 app.use(express.json());
+app.use(express.static(CONSTANTS.PUBLIC_FILE_PATH));
 app.use(router);
 
 // const basicErrorHandler = require('./middlewares/errors');
---
+++ constants.js
@@ -0,0 +1,7 @@
+const { path } = require("./app");
+
+const CONSTANTS = {
+  PUBLIC_FILE_PATH: path.resolve(__dirname, "public")
+}
+
+module.exports = CONSTANTS;
