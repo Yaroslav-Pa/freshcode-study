82ef670d2b5489b52735b9f94b55a12b28bc7dfb
Yaroslav-Pa <yaroslav.roro@gmail.com>
2024-03-28 16:17:39 +0200

    clearing

--- constants.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-
-const CONSTANTS = {
-  PUBLIC_FILE_PATH : path.resolve(__dirname, 'public')
-}
-
-module.exports = CONSTANTS;
--- routers/index.js
+++
@@ -1,8 +0,0 @@
-const superhumanRouter = require('./superhumans');
-const SuperpowerController = require('../controllers/superpowersController');
-const router = require('express').Router();
-
-router.use('/superhumans', superhumanRouter);
-router.get('/superpowers', SuperpowerController.getAllSuperpowers);
-
-module.exports = router;
